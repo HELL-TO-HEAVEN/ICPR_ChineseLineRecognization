f340a1db51299d2dbbee48591072ba74c8ec11c0
Yindong Zhang <so_go_flying@163.com>
2018-11-09 13:57:35 +0800

    1. add train/validation split. 2. add resize function to preprocess test image.

--- src/crop.py
+++ src/crop.py
@@ -3,6 +3,7 @@ import numpy as np
 import os
 import math
 import cv2
+from sklearn.model_selection import ShuffleSplit
 import codecs
 from config import  log
 
@@ -19,32 +20,36 @@ def clock(coor):
 
     return coor[0], coor[1]
 
-def crop():
+def preprocess():
     root_dir = '../data/originData'
     text_dir = os.path.join(root_dir, 'txt_train')
     img_dir = os.path.join(root_dir, 'image_train')
-    save_dir_hor = os.path.join(root_dir, 'crop_img_hor')
-    save_dir_ver = os.path.join(root_dir, 'crop_img_ver')
+    shuffleSplit = ShuffleSplit(n_splits=1, test_size=0.1)
+    imgList = os.listdir(img_dir)
+    for trainInd, valInd in shuffleSplit.split(imgList):
+        trainDir= '../data/train'
+        valDir= '../data/val'
+        log.info('Train image crop Start:')
+        crop([imgList[i] for i in trainInd], img_dir, text_dir, trainDir)
+        log.info('Validation image crop Start:')
+        crop([imgList[i] for i in valInd], img_dir, text_dir, valDir)
+
+def crop(imgFiles, imgDir, txtDir, saveDir):
+    save_dir_hor = os.path.join(saveDir, 'crop_img_hor')
+    save_dir_ver = os.path.join(saveDir, 'crop_img_ver')
     if not os.path.exists(save_dir_hor):
         os.mkdir(save_dir_hor)
     if not os.path.exists(save_dir_ver):
         os.mkdir(save_dir_ver)
-    i = 0
     fh = codecs.open(save_dir_hor + '/label.txt', 'w', encoding='utf-8')
     fv = codecs.open(save_dir_ver + '/label.txt', 'w', encoding='utf-8')
-    for imgfile in os.listdir(img_dir):
-        i = i + 1
-        img = Image.open(os.path.join(img_dir, imgfile)).convert('RGB')
-        txtname = imgfile.split('.')[0:-1]
-        imgname = '.'.join(txtname)
-        txtname.append('txt')
-        txtname = '.'.join(txtname)
 
-        # For test:
-        if i == 102:
-            break
-
-        with open(os.path.join(text_dir, txtname), 'r', encoding="utf-8") as f:
+    horCount= verCount= 0
+    for imgFile in imgFiles:
+        img = Image.open(os.path.join(imgDir, imgFile)).convert('RGB')
+        imgName, _= os.path.splitext(imgFile)
+        txtName= imgName + '.txt'
+        with open(os.path.join(txtDir, txtName), 'r', encoding="utf-8") as f:
             for j, line in enumerate(f.readlines()):
                 txt = line.split(',')
                 coordinates = [float(i) for i in txt[0:8]]
@@ -66,34 +71,73 @@ def crop():
                 M = cv2.getPerspectiveTransform(Pts1, Pts2)
                 dst = cv2.warpPerspective(img, M, (int(w), int(h)))
                 img_new = Image.fromarray(dst)
-                # name = '/'+str(i)+'_'+str(j)+'.jpg'
-                name = imgname + '_' + str(j) + '.jpg'
-                if w > h: # 横的图片
+                name = imgName + '_' + str(j) + '.jpg'
+                if w > h:  # 横的图片
                     # 对image_new做resize
+                    horCount += 1
                     p = img_new.size[1] / 31
-                    if p < 1:
-                        continue
+                    # if p < 1:
+                    #     continue
                     new_height = 31
                     new_width = int(img_new.size[0] / p)
-                    log.debug('horizonal image: with width %+3s and height %+3s' %(new_width, new_height))
+                    log.debug('horizonal image: with width %+3s and height %+3s' % (new_width, new_height))
                     image_resized = img_new.resize((new_width, new_height))
 
                     image_resized.save(os.path.join(save_dir_hor, name))
                     fh.write(name + ' ' + label + '\n')
-                else: # 竖的图片
+                else:  # 竖的图片
+                    verCount += 1
                     p = img_new.size[0] / 31
-                    if p < 1:
-                        continue
+                    # if p < 1:
+                    #     continue
                     new_height = int(img_new.size[1] / p)
                     new_width = 31
-                    log.debug('vertical image: with width %+3s and height %+3s' %(new_width, new_height))
+                    log.debug('vertical image: with width %+3s and height %+3s' % (new_width, new_height))
                     image_resized = img_new.resize((new_width, new_height))
 
                     image_resized.save(os.path.join(save_dir_ver, name))
                     fv.write(name + ' ' + label + '\n')
-    # break
+        # break
     fh.close()
     fv.close()
+    sumCount= horCount + verCount
+    log.info('%s images cropped in total with %s horizational images and %s vertical images' %(sumCount, horCount, verCount))
+
+
+def resize(imgDir, saveDir):
+    root_dir = '../data/originData/image_val'
+    save_dir_hor = os.path.join(saveDir, 'img_hor_resized')
+    save_dir_ver = os.path.join(saveDir, 'img_ver_resized')
+    if not os.path.exists(save_dir_hor):
+        os.makedirs(save_dir_hor)
+    if not os.path.exists(save_dir_ver):
+        os.makedirs(save_dir_ver)
+
+    for i, imgName in enumerate(os.listdir(imgDir)):
+        log.info("%s'th image opened" %(i, ))
+        img = Image.open(os.path.join(imgDir, imgName)).convert('RGB')
+        if img.size[0] > img.size[1]: # 横的图片
+            p = img.size[1] / 31
+            # if p < 1:
+            #     continue
+            new_height = 31
+            new_width = int(img.size[0] / p)
+            log.debug('horizonal image: with width %+3s and height %+3s' % (new_width, new_height))
+            image_resized = img.resize((new_width, new_height))
+            image_resized.save(os.path.join(save_dir_hor, imgName))
+        else:
+            p = img.size[0] / 31
+            # if p < 1:
+            #     continue
+            new_height = int(img.size[1] / p)
+            new_width = 31
+            log.debug('vertical image: with width %+3s and height %+3s' % (new_width, new_height))
+            image_resized = img.resize((new_width, new_height))
+
+            image_resized.save(os.path.join(save_dir_ver, imgName))
+    log.info("Image Resize succeed! %s in total." %(i+ 1, ))
+
 
 if __name__ == '__main__':
-    crop()
+    preprocess()
+    # resize("../data/originData/icpr_mtwi_task1/test_line_image")
